95e1f6cdba6877cebec9fa90578b7a1a14794919
Shefali-Sharma <shefali92@gmail.com>
2018-07-18 12:44:47 +0530

    Implementing Sigmoid Gradient
    Implementing Sigmoid Gradient in sigmoidGradient.m file.

--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -12,18 +12,10 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
+temp = 1 + exp(-z);
+gz = 1 ./ temp;
 
-
-
-
-
-
-
-
-
-
-
-
+g = gz .* (1-gz);
 
 % =============================================================
 
