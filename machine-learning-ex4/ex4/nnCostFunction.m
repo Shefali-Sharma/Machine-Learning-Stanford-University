b5f937a03bea5e4e097806c047c371e7c1571de1
Shefali-Sharma <shefali92@gmail.com>
2018-07-18 15:36:40 +0530

    Implement Regularization
    Implement regularization with the cost function and gradients

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -109,6 +109,16 @@ delta2 = d3' * a2;
 Theta1_grad = Theta1_grad + (1/m) * delta1;
 Theta2_grad = Theta2_grad + (1/m) * delta2;
 
+% Part 3: Implement regularization with the cost function and gradients.
+%
+%         Hint: You can implement this around the code for
+%               backpropagation. That is, you can compute the gradients for
+%               the regularization separately and then add them to Theta1_grad
+%               and Theta2_grad from Part 2.
+%
+
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m)*Theta1;
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda/m)*Theta2;
 
 % grad = my_grad + ((lambda/m) .* theta);
 
